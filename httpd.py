c296255ba7757ff918c8c961258259a7fd720b9b
valed-dm <dmvaled@gmail.com>
2023-08-08 16:56:04 +0300

    GET, HEAD requests set up

--- httpd.py
+++ httpd.py
@@ -21,17 +21,31 @@ def on_read_handler(sel, sock, addr):
     if not data:
         print("Disconnected by", addr)
         return False
+
+    method = data.decode().split(" ")[0]
+    print("method ===>", method)
+
     try:
-        sock.send(bytes("HTTP/1.0 200 OK\r\n", "utf-8"))
-        sock.send(bytes("Content-Type: text/html\r\n\r\n", "utf-8"))
-        sock.send(bytes(f"Content-Length: {len(data)}\r\n\r\n", "utf-8"))
-        sock.send(data)
-        sock.close()
-        sel.unregister(sock)
-        # print(f"Send: {data} to: {addr}")
+        if method == "GET":
+            sock.send(bytes("HTTP/1.1 200 OK\r\n", "utf-8"))
+            sock.send(bytes("Content-Type: text/html\r\n\r\n", "utf-8"))
+            sock.send(bytes(f"Content-Length: {len(data)}\r\n\r\n", "utf-8"))
+            sock.send(data)
+        elif method == "HEAD":
+            sock.send(bytes("HTTP/1.1 204 OK\r\n", "utf-8"))
+            sock.send(bytes(f"Content-Length: {len(data)}\r\n\r\n", "utf-8"))
+        else:
+            sock.send(bytes("HTTP/1.1 405 Not Allowed\r\n", "utf-8"))
+            sock.send(bytes("Content-Type: text/html\r\n\r\n", "utf-8"))
+
     except ConnectionError:
         print("Client suddenly closed, cannot send")
+        sock.close()
+        sel.unregister(sock)
         return False
+
+    sock.close()
+    sel.unregister(sock)
     return True
 
 
