41ce7a90ed6fdc04ecd4c8d5a1182027ab3b8fa3
valed-dm <dmvaled@gmail.com>
2023-08-11 18:06:52 +0300

    on_read.py refactored
    sock.close();
    requested file is closed when response completed;

--- handlers/on_read.py
+++ handlers/on_read.py
@@ -1,5 +1,6 @@
-from utils import time_now_rfc_1123, data_decode
 import os
+
+from utils import time_now_rfc_1123, data_decode
 
 
 def on_read_handler(sel, sock, addr):
@@ -16,32 +17,27 @@ def on_read_handler(sel, sock, addr):
     method, file = data_decode(data)
     file_path = os.path.abspath("DOCUMENT_ROOT/httptest/" + str(file))
     file_size = os.path.getsize(file_path)
-    file_requested = open(file_path, "rb")
 
-    print("method:", method, "\nfile:", file, "\nsize:", file_size)
-
-    try:
-        if method == "GET":
-            sock.send(bytes("HTTP/1.1 200 OK\r\n", "utf-8"))
-            sock.send(bytes(f"Date: {now}\r\n", "utf-8"))
-            sock.send(bytes("Server: socket/1.0\r\n", "utf-8"))
-            sock.send(bytes(f"Content-Length: {file_size}\r\n", "utf-8"))
-            sock.send(bytes("Content-Type: text/html\r\n", "utf-8"))
-            sock.send(bytes("Connection: close\r\n\r\n", "utf-8"))
-            sock.send(bytes(file_requested.read()))
-        elif method == "HEAD":
-            sock.send(bytes("HTTP/1.1 204 OK\r\n", "utf-8"))
-            sock.send(bytes(f"Content-Length: {file_size}\r\n\r\n", "utf-8"))
-        else:
-            sock.send(bytes("HTTP/1.1 405 Not Allowed\r\n", "utf-8"))
-            sock.send(bytes("Content-Type: text/html\r\n\r\n", "utf-8"))
-
-    except ConnectionError:
-        print("Client suddenly closed, cannot send")
-        sock.close()
-        sel.unregister(sock)
-        return False
-
-    sock.close()
-    sel.unregister(sock)
-    return True
+    with open(file_path, "rb") as f:
+        try:
+            if method == "GET":
+                sock.send(bytes("HTTP/1.1 200 OK\r\n", "utf-8"))
+                sock.send(bytes(f"Date: {now}\r\n", "utf-8"))
+                sock.send(bytes("Server: socket/1.0\r\n", "utf-8"))
+                sock.send(bytes(f"Content-Length: {file_size}\r\n", "utf-8"))
+                sock.send(bytes("Content-Type: text/html\r\n", "utf-8"))
+                sock.send(bytes("Connection: close\r\n\r\n", "utf-8"))
+                sock.send(bytes(f.read()))
+            elif method == "HEAD":
+                sock.send(bytes("HTTP/1.1 204 OK\r\n", "utf-8"))
+                sock.send(bytes(f"Content-Length: {file_size}\r\n\r\n", "utf-8"))
+            else:
+                sock.send(bytes("HTTP/1.1 405 Not Allowed\r\n", "utf-8"))
+                sock.send(bytes("Content-Type: text/html\r\n\r\n", "utf-8"))
+        except ConnectionError:
+            print("Client suddenly closed, cannot send")
+            return False
+        finally:
+            sock.close()
+            sel.unregister(sock)
+        return True
