4972fc39249488a5016348e65f656ef70aa26bc0
valed-dm <dmvaled@gmail.com>
2023-08-12 12:13:57 +0300

    find_path_to_file.py recursion added

---
+++ utils/find_path_to_file.py
@@ -0,0 +1,10 @@
+import fnmatch
+import os
+
+
+def find_path(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                return filename
