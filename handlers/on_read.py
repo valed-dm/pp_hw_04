dc4ffc9c5ed985272882ea18adbd7c577b0bfab7
valed-dm <dmvaled@gmail.com>
2023-08-13 06:25:04 +0300

    404 Not Found case processed

--- handlers/on_read.py
+++ handlers/on_read.py
@@ -1,6 +1,5 @@
-import os
-
-from utils import time_now_rfc_1123, data_decode, file_path
+from .on_file import on_file
+from utils import time_now_rfc_1123, data_decode
 
 
 def on_read_handler(sel, sock, addr, root):
@@ -19,25 +18,24 @@ def on_read_handler(sel, sock, addr, root):
     # prevents server down when browser asks for favicon.ico
     if file == "favicon.ico":
         sock.send(bytes("HTTP/1.1 200 OK\r\n", "utf-8"))
-        sock.close()
-        sel.unregister(sock)
-        return True
-
-    fp = file_path(root, file)
-    file_size = os.path.getsize(fp)
+        return False
+    response, fp, file_size, file_exists = on_file(root, file)
 
     with open(fp, "rb") as f:
         try:
             if method == "GET":
-                sock.send(bytes("HTTP/1.1 200 OK\r\n", "utf-8"))
+                sock.send(bytes(response, "utf-8"))
                 sock.send(bytes(f"Date: {now}\r\n", "utf-8"))
                 sock.send(bytes("Server: socket/1.0\r\n", "utf-8"))
                 sock.send(bytes(f"Content-Length: {file_size}\r\n", "utf-8"))
                 sock.send(bytes("Content-Type: text/html\r\n", "utf-8"))
                 sock.send(bytes("Connection: close\r\n\r\n", "utf-8"))
                 sock.send(bytes(f.read()))
-            elif method == "HEAD":
+            elif method == "HEAD" and file_exists:
                 sock.send(bytes("HTTP/1.1 204 OK\r\n", "utf-8"))
+                sock.send(bytes(f"Content-Length: {file_size}\r\n\r\n", "utf-8"))
+            elif method == "HEAD" and not file_exists:
+                sock.send(bytes(response, "utf-8"))
                 sock.send(bytes(f"Content-Length: {file_size}\r\n\r\n", "utf-8"))
             else:
                 sock.send(bytes("HTTP/1.1 405 Not Allowed\r\n", "utf-8"))
