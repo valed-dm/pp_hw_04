36e53e15997e718284a3c72e36f09325d530511d
valed-dm <dmvaled@gmail.com>
2023-08-15 22:35:01 +0300

    data_decode.py modified to pass
    query string after filename
    filename with spaces
    urlencoded filename

--- utils/data_decode.py
+++ utils/data_decode.py
@@ -1,5 +1,41 @@
+from urllib.parse import unquote
+
+from .fn_replace import fn_replace
+from .smart_rstrip import smart_rstrip
+
+
 def data_decode(data):
+    """Decodes HTTP request data"""
+
     d = data.decode().split(" ")
-    method = d[0]
-    file = d[1].lstrip("/?=")
-    return method, file
+    if d:
+        method = d[0] if d[0] in ["GET", "HEAD"] else None
+        try:
+            file = d[1].lstrip("/?=")
+            file = smart_rstrip(file, char="?")
+
+            if "%" in file:
+                file_split = file.split("/")
+                percent_encoded = file_split[-1]
+                print("percent_encoded =>", percent_encoded)
+
+                if "." in percent_encoded:
+                    space_inserted = percent_encoded.replace("%20", " ")
+                    file = fn_replace(
+                        fplist=file_split, fn_old=percent_encoded, fn_new=space_inserted
+                    )
+                    return method, file
+
+                try:
+                    decoded = unquote(percent_encoded)
+                    file = fn_replace(
+                        fplist=file_split, fn_old=percent_encoded, fn_new=decoded
+                    )
+                    return method, file
+                except ValueError:
+                    file = None
+
+        except IndexError:
+            file = None
+
+        return method, file
