7d58c525eb77f03a0b8cebf28abad6bacdeb2836
valed-dm <dmvaled@gmail.com>
2023-08-15 22:30:04 +0300

    smart_rstrip.py added

---
+++ utils/smart_rstrip.py
@@ -0,0 +1,9 @@
+def smart_rstrip(string, char):
+    """Right strips string from first occurrence from given char"""
+
+    try:
+        idx = string.index(char)
+        string = string[:idx]
+        return string
+    except ValueError:
+        return string
